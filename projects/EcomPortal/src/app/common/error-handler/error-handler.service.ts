6df8713a5580e83ee1fc101f8b8757cefc81745a
Santosh Yadav <santosh.yadav198613@gmail.com>
2019-04-14 14:56:50 +0530

    error logging

---
+++ projects/EcomPortal/src/app/common/error-handler/error-handler.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, ErrorHandler, Injector } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { APP_CONFIG } from '../../material-shared/AppConfig';
+import { IAppConfig } from '../../material-shared/IAppConfig';
+import { MatSnackBar } from '@angular/material';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorHandlerService implements ErrorHandler {
+
+  constructor(private injector: Injector) { }
+
+  handleError(error: any): void {
+    if (error != null || error !== '') {
+      const http = this.injector.get(HttpClient);
+      const appConfig = this.injector.get(APP_CONFIG);
+      const snackBar = this.injector.get(MatSnackBar);
+      const errorMessage = JSON.stringify(error);
+      http.post(appConfig.apiEndPoint + '/errorLog', { error: errorMessage }).subscribe((result) => {
+        snackBar.open('There is some problem performing this opertion, please try again!', 'Error!', {
+          duration: 1000
+        });
+      });
+    }
+  }
+}
